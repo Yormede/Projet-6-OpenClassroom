ffd852dbe6dbacc9e6560f9df78660bc5fff1e4a
Yormede <ahmedsvg69@gmail.com>
2023-07-24 19:42:41 +0200

    modification padding collapse modification margin dans la page lodging ajout de l underline pour les nav du header

--- kasa/src/components/header/Header.js
+++ kasa/src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './_header.scss'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from "../../images/LOGO.svg";
 
 const Header = () => {
@@ -10,8 +10,8 @@ const Header = () => {
             <img src={logo} alt='Logo de Kasa'/>
             <nav>
                 <ul>
-                    <Link to="/"><li>Accueil</li></Link>
-                    <Link to="About"><li >A propos</li></Link>
+                    <NavLink to="/"><li>Accueil</li></NavLink>
+                    <NavLink to="About"><li >A propos</li></NavLink>
                 </ul>
             </nav>
         </header>
